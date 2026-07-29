19192fa759c0be2a061718895f58c6b72703f76d
XUMU <xmgeorge06@gmail.com>
2026-07-29 23:56:29 +0300

    Create initial sponsor management page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,8 @@
-
+export default function Home() {
+    return (
+        <main>
+            <h1>Sponsor Management</h1>
+            <p>Εφαρμογή διαχείρισης χορηγών</p>
+        </main>
+    );
+}
